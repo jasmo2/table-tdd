3db658c19153626528a82419e66b07d7fdbb225f
Jaime <jasmo2.com@gmail.com>
2021-02-23 12:20:46 -0500

    initial commit

---
+++ src/Table/table.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Table from './Table'
+
+describe('Table', () => {
+  test('renders Table properly', () => {
+    render(<Table />)
+  })
+})
