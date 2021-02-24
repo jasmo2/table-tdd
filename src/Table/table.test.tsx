fe992bf9c84396f2459047d0aaf58f393e199284
Jaime <jasmo2.com@gmail.com>
2021-02-24 10:07:45 -0500

    Header input

--- src/Table/table.test.tsx
+++ src/Table/table.test.tsx
@@ -2,18 +2,23 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Table from './table'
 
-const columns = ['Name', 'Surname', 'Phone', 'Actions']
+const columns = [
+  { Header: 'Name', accessor: 'name' },
+  { Header: 'Surname', accessor: 'surname' },
+  { Header: 'Phone', accessor: 'phone' },
+  { Header: 'Actions', accessor: 'actions' },
+]
 
 describe('Table', () => {
   test('renders Table properly', () => {
-    render(<Table />)
+    render(<Table columns={columns} />)
   })
 
   test('Header base on input', () => {
     render(<Table columns={columns} />)
 
     for (const column of columns) {
-      const header = screen.getByText(new RegExp(column))
+      const header = screen.getByText(new RegExp(column.Header))
       expect(header).not.toBeNull()
     }
   })
--- src/Table/table.tsx
+++ src/Table/table.tsx
@@ -1,14 +1,23 @@
 import React, { FC } from 'react'
 import { STable } from './Table.styles'
+import { Accessor, Column } from 'react-table'
+type tColumn = {
+  Header: string
+  accessor: string
+}
 
 type tTable = {
-  columns: string[]
+  columns: tColumn[]
 }
 
 const Table: FC<tTable> = ({ columns }) => {
   return (
     <STable>
-      <header>{columns.map((col) => col)}</header>
+      <header>
+        {columns.map(({ Header, accessor }, i) => (
+          <h3 key={`header-${i}`}>{Header}</h3>
+        ))}
+      </header>
     </STable>
   )
 }
